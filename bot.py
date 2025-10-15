cd6146fba990d5bfce5032b5b51ca1b1fc990115
Daniel <danielgbarrow@gmail.com>
2025-10-15 19:44:12 +0100

    done

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import discord
 import os
 from dotenv import load_dotenv
-from discord.ext import commands
-from discord.ext import tasks
 import discord.utils
 from datetime import datetime, timedelta
 
@@ -11,7 +9,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 intents.presences = True
-bot = commands.Bot(command_prefix="/", intents=intents)
+bot = discord.Bot()
 bottoken = os.environ["DISCORD_TOKEN"]
 
 
